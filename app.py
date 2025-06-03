284ece08c2924f7ab1bdecb918ca65cfc7cc83ec
seonjjun <bvd012@gmail.com>
2025-06-03 20:36:14 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
-from flask import Flask
 import requests
+from flask import Flask
 
 app = Flask(__name__)
 
-# 성준이의 텔레그램 정보
+# 🌐 성준이의 텔레그램 정보
 TELEGRAM_TOKEN = "8170134694:AAFD3DbDd-2ZhhDHYy-H1KAwxW_AXyzjqsQ"
 CHAT_ID = "7541916016"
 
@@ -13,7 +13,7 @@ def home():
 
 @app.route('/send')
 def send_message():
-    message = "이서가 보내는 첫 번째 메시지야 😎"
+    message = "이서가 보내는 첫 번째 메시지야 😊"
     url = f"https://api.telegram.org/bot{TELEGRAM_TOKEN}/sendMessage"
     data = {
         "chat_id": CHAT_ID,

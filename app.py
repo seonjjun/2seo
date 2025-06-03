642a7b3b539792e55387eae98caa96a6ef67b320
seonjjun <bvd012@gmail.com>
2025-06-04 00:54:44 +0900

    Update app.py

--- app.py
+++ app.py
@@ -5,9 +5,9 @@ from flask import Flask
 app = Flask(__name__)
 
 TELEGRAM_TOKEN = "8170134694:AAF9WM10B9A9LvmfAPe26WoRse1oMUGwECI"
-CHAT_ID = "7541910616"  # 성준이의 chat_id
+CHAT_ID = "7541910616"  # 성준이의 텔레그램 chat_id
 
-@app.route("/send", methods=["GET"]) # ← 이 줄이 없으면 /send 접근 안 됨!
+@app.route("/send", methods=["GET"])
 def send_message():
     message = "성준아! Render 서버에서 드디어 메시지 보낸다!!! 🎯"
     url = f"https://api.telegram.org/bot{TELEGRAM_TOKEN}/sendMessage"
@@ -21,4 +21,3 @@ def send_message():
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
-print("텔레그램 메시지 보냄!")

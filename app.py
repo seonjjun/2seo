b831a686ed344e7b333346c61f3b12f9678ec0c8
seonjjun <bvd012@gmail.com>
2025-06-03 19:47:06 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -6,4 +7,4 @@ def home():
     return "2seo Alert Bot is running!"
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0', port=10000)

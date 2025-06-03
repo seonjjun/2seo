746360d66d4b96d3a5cc1480dbb8de9e47d5a9eb
seonjjun <bvd012@gmail.com>
2025-06-04 01:23:54 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,22 +1,25 @@
 import os
 import requests
 from flask import Flask
+from dotenv import load_dotenv
+
+load_dotenv()  # .env 파일의 내용 불러오기
 
 app = Flask(__name__)
 
-TELEGRAM_TOKEN = "8170134694:AAF9WM10B9A9LvmfAPe26WoRse1oMUGwECI"
-CHAT_ID = "7541916016"  # 성준이의 텔레그램 chat_id
+TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
+CHAT_ID = os.getenv("CHAT_ID")
 
 @app.route("/send", methods=["GET"])
 def send_message():
-    message = "성준아! Render 서버에서 드디어 메시지 보낸다!!! 🎯🚀"
+    message = "✅ 성준아! Render 서버에서 드디어 메시지 보낸다!!! 🎯🛠️"
     url = f"https://api.telegram.org/bot{TELEGRAM_TOKEN}/sendMessage"
     data = {
         "chat_id": CHAT_ID,
         "text": message
     }
     requests.post(url, json=data)
-    return "✅ 메시지 전송 완료!"
+    return "📨 메시지 전송 완료!"
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))

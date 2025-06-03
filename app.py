e9dd66513a887a6b21635aa7ae4b3d0ea2677d48
seonjjun <bvd012@gmail.com>
2025-06-03 20:05:52 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,24 @@
+import requests
 from flask import Flask
 
 app = Flask(__name__)
+
+# 👉 성준이의 텔레그램 봇 정보
+TELEGRAM_TOKEN = "8170134694:AAFD3DbDd-2ZhhDHYy-H1KAwxW_AXyzjqsQ"
+CHAT_ID = "7541916016"
 
 @app.route('/')
 def home():
     return "2seo Alert Bot is running!"
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=10000)
+@app.route('/send')
+def send_message():
+    message = "이서가 보내는 첫 번째 메시지야 😊"
+    url = f"https://api.telegram.org/bot{TELEGRAM_TOKEN}/sendMessage"
+    data = {
+        "chat_id": CHAT_ID,
+        "text": message
+    }
+    response = requests.post(url, data=data)
+    return f"보낸 결과: {response.json()}"
+
